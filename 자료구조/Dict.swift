aee4c4c643d970f8f0a81534c6336f0aef743b00
yongbeomkwak <yongbeomkwak@gmail.com>
2023-09-10 19:47:10 +0900

    :sparkles:: 정렬 관련 추가

--- 자료구조/Dict.swift
+++ 자료구조/Dict.swift
@@ -4,3 +4,11 @@
 let myDict = ["a" : 1, "b" : 2, "c" : 3]
 
 print(myDict["d", default: -1])
+
+
+//forEach  $0.key ,$0.value로 접근
+
+ parkingLot.forEach { // 아직 출차하지 않은 차 출차 
+        print($0.key)
+        print($0.value)
+}
