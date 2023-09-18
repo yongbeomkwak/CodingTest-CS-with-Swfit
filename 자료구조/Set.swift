8f8b8803b9b48b2e09f1cbf1af97ce5e72e39c86
yongbeomkwak <yongbeomkwak@gmail.com>
2023-09-18 14:27:43 +0900

    Set

---
+++ 자료구조/Set.swift
@@ -0,0 +1,60 @@
+// 1. Set 생성 및 선언
+var integerSet: Set<Int> = Set<Int>()
+
+// insert : 새로운 멤버 입력
+// 동일한 값은 여러번 insert해도 한번만 저장됩니다.
+integerSet.insert(1)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(100)
+
+print(intigerSet) // {100, 99, 1}
+
+// contains: 멤버 포함 여부 확인
+print(integerSet.contatins(1)) // true
+print(integerSet.contains(2)) // false
+
+// remove: 멤버 삭제
+integerSet.remove(99) // {100, 1}
+integerSet.removeFirst() // {1}
+
+// count: 멤버 개수
+integerSet.count // 1
+
+
+// 2. Set의 활용
+// 멤버의 유일성이 보장되기 때문에 집합 연산에 활용하면 유용합니다.
+let setA: Set<Int> = [1, 2, 3, 4, 5]
+let setB: Set<Int> = [3, 4, 5, 6, 7]
+
+// 합집합
+let union: Set<Int> = setA.union(setB)
+print(union) // [2, 4, 5, 6, 7, 3, 1]
+
+// 합집합 오름차순 정렬
+let sortedUnion: [Int] = union.sorted()
+print(sortedUnion) // [1, 2, 3, 4, 5, 6, 7]
+
+// 교집합
+let intersection: Set<Int> = setA.intersection(setB)
+print(intersection) // [5, 3, 4]
+
+// 차집합
+let subtracting: Set<Int> = setA.subtracting(setB)
+print(subtracting) // [2, 1]
+
+
+// 부분 집합
+
+let setA = Set<Int>(1..3) // 1,2,3
+let setB = Set<Int>(2..3) // 2,3
+
+print(setB.isSubset(of:setA) // true
+print(setA.isSubset(of:setB) // false
+
+// 차 집합 
+print(setB.subtracting(setA)) // [1]
+
+//대칭 차집합 , 합집합에서 교집합을 뺀것이라고 생각하면된다. 
+print(setB.symmetricDifference(setA)) // 
