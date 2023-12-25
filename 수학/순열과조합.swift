09d742aad647e003a57d54361f79e239c9cc4e93
yongbeomkwak <yongbeomkwak@gmail.com>
2023-12-25 16:02:21 +0900

    중복조합 중복순열

--- 수학/순열과조합.swift
+++ 수학/순열과조합.swift
@@ -1,25 +1,3 @@
-// 조합 
-// nCr
-func combination<T>(_ elements: [T], _ k: Int) -> [[T]] {
-    var result = [[T]]()
-    
-    func combi(_ index: Int, _ now: [T]) {
-        if now.count == k {
-            result.append(now)
-            return
-        }
-        for i in index..<elements.count {
-            combi(i + 1, now + [elements[i]])
-        }
-    }
-    combi(0, [])
-    return result
-}
-
-let arr = [1, 2, 3, 4]
-let combi = combination(arr, 3)
-    
-
 // 순열
 
 func permutation<T>(_ elements: [T], _ k: Int) -> [[T]] {
@@ -45,3 +23,63 @@ func permutation<T>(_ elements: [T], _ k: Int) -> [[T]] {
 
 let arr = [1, 2, 3, 4]
 let permu = permutation(arr, 3)
+
+
+// 조합 
+// nCr
+func combination<T>(_ elements: [T], _ k: Int) -> [[T]] {
+    var result = [[T]]()
+    
+    func combi(_ index: Int, _ now: [T]) {
+        if now.count == k {
+            result.append(now)
+            return
+        }
+        for i in index..<elements.count {
+            combi(i + 1, now + [elements[i]])
+        }
+    }
+    combi(0, [])
+    return result
+}
+
+let arr = [1, 2, 3, 4]
+let combi = combination(arr, 3)
+    
+
+// 중복 순열  nPIr
+func Rpermutation<T>(_ elements: [T], _ k: Int) -> [[T]] {
+    var result = [[T]]()
+    var visited = [Bool](repeating: false, count: elements.count)
+    
+    func Rpermute(_ now: [T]) {
+        if now.count == k {
+            result.append(now)
+            return
+        }
+        
+        for i in 0..<elements.count {
+            Rpermut(now + [elements[i]])
+        }
+    }
+    Rpermut([])
+    return result
+}
+
+// 중복 조합
+// nHr
+func Rcombination<T>(_ elements: [T], _ k: Int) -> [[T]] {
+    var result = [[T]]()
+    
+    func Rcombi(_ index: Int, _ now: [T]) {
+        if now.count == k {
+            result.append(now)
+            return
+        }
+        for i in 0..<elements.count {
+            Rcombi(i + 1, now + [elements[i]])
+        }
+    }
+    Rcombi(0, [])
+    return result
+}
