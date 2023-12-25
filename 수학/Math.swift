30a850ec911b286e2d7ff4d1a1fbabb2c37fb704
yongbeomkwak <yongbeomkwak@gmail.com>
2023-12-25 15:33:37 +0900

    순열과 조합 분리:

--- 수학/Math.swift
+++ 수학/Math.swift
@@ -46,50 +46,19 @@ floor(-3.658) // -4.0
 
 
 
-// 조합 
-// nCr
-func combination<T>(_ elements: [T], _ k: Int) -> [[T]] {
-    var result = [[T]]()
-    
-    func combi(_ index: Int, _ now: [T]) {
-        if now.count == k {
-            result.append(now)
-            return
-        }
-        for i in index..<elements.count {
-            combi(i + 1, now + [elements[i]])
-        }
+
+
+
+/// 최대공약수
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
     }
-    combi(0, [])
-    return result
 }
 
-let arr = [1, 2, 3, 4]
-let combi = combination(arr, 3)
-    
-
-// 순열
-
-func permutation<T>(_ elements: [T], _ k: Int) -> [[T]] {
-    var result = [[T]]()
-    var visited = [Bool](repeating: false, count: elements.count)
-    
-    func permut(_ now: [T]) {
-        if now.count == k {
-            result.append(now)
-            return
-        }
-        
-        for i in 0..<elements.count {
-            if visited[i] == true { continue }
-            visited[i] = true
-            permut(now + [elements[i]])
-            visited[i] = false
-        }
-    }
-    permut([])
-    return result
+/// 최소공배수
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a * b / gcd(a, b)
 }
-
-let arr = [1, 2, 3, 4]
-let permu = permutation(arr, 3)
---
+++ 수학/순열과조합.swift
@@ -0,0 +1,47 @@
+// 조합 
+// nCr
+func combination<T>(_ elements: [T], _ k: Int) -> [[T]] {
+    var result = [[T]]()
+    
+    func combi(_ index: Int, _ now: [T]) {
+        if now.count == k {
+            result.append(now)
+            return
+        }
+        for i in index..<elements.count {
+            combi(i + 1, now + [elements[i]])
+        }
+    }
+    combi(0, [])
+    return result
+}
+
+let arr = [1, 2, 3, 4]
+let combi = combination(arr, 3)
+    
+
+// 순열
+
+func permutation<T>(_ elements: [T], _ k: Int) -> [[T]] {
+    var result = [[T]]()
+    var visited = [Bool](repeating: false, count: elements.count)
+    
+    func permut(_ now: [T]) {
+        if now.count == k {
+            result.append(now)
+            return
+        }
+        
+        for i in 0..<elements.count {
+            if visited[i] == true { continue }
+            visited[i] = true
+            permut(now + [elements[i]])
+            visited[i] = false
+        }
+    }
+    permut([])
+    return result
+}
+
+let arr = [1, 2, 3, 4]
+let permu = permutation(arr, 3)
