1236cc50148c9f77beb94ab89d5eb200c0ff6c9f
yongbeomkwak <yongbeomkwak@gmail.com>
2024-02-10 19:27:27 +0900

    비트 연산

---
+++ 비트연산/비트연산자.swift
@@ -0,0 +1,5 @@
+// 100
+print(4.nonzeroBitCount) // 1의 개수 1
+print(4.bitWidth) // 64 bit
+print(4.leadingZeroBitCount) // 1 왼쪽에 있는 0개수  bitWidth - trailingZeroBitCount - nonzeroBitCount   =  64 -1 -2 = 61
+print(4.trailingZeroBitCount) // 1오른쪽에 있는 0개수 = 2
