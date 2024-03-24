ffc3f1f65fead8009aeb18c5778d580dd3de2f42
Hamp <yongbeomkwak@gmail.com>
2024-03-24 20:27:47 +0900

    이분탐색

---
+++ 이분탐색/이분탐색.swift
@@ -0,0 +1,25 @@
+func lowerBound(_ arr: [Int], _ x: Int) -> Int {
+  var start = 0
+  var end = arr.count - 1
+  
+  while (start < end) {
+    let mid = (start + end) / 2
+    if arr[mid] >= x { end = mid }
+    else { start = mid + 1}
+  }
+
+  return end
+}
+
+func upperBound(_ arr: [Int], _ x: Int) -> Int {
+  var start = 0
+  var end = arr.count - 1
+  
+  while (start < end) {
+    let mid = (start + end) / 2
+    if arr[mid] > x { end = mid }
+    else { start = mid + 1}
+  }
+
+  return end
+}
